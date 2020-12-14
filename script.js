ed77e2a82bf38068ae80a9aad95e320690913279
Léa Benovici <lea.benovici@gmail.com>
2020-12-14 15:14:21 +0100

    ajout btn haut de page

--- script.js
+++ script.js
@@ -275,3 +275,24 @@ function ValidationR() {
     }
 }
 
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    console.log("dans la fonction")
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton.style.display = "block";
+        console.log("dans le if")
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
